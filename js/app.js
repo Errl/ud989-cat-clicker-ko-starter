d8ac0466d7c6f2228a695cbb1a7d0ad8d6ca831f
Hanane Moshe <hananemo@gmail.com>
2015-03-18 19:44:38 -0400

    completed catclicker knockout.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,61 @@
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://flickr.come/photos/big');
+var initialCats = [
+        {
+            name: 'Molly',
+            imgSrc: 'images/molly.jpg',
+            imgAttribution: 'https://flickr.come/photos/big',
+            clickCount: 0,
+            nickNames: ['Tabbs', 'Babbs', 'Tabitha', 'mo']
+        },
 
-    this.nickName = ko.observableArray([
-        { nick: 'Tabbs' },
-        { nick: 'Babbs' },
-        { nick: 'Tabitha' },
-        { nick: 'mo' }
-    ]);
+        {
+            name: 'Henry',
+            imgSrc: 'images/cat1.jpg',
+            imgAttribution: 'https://flickr.come/photos/big',
+            clickCount: 0,
+            nickNames: ['Tabbs', 'Babbs', 'Tabitha', 'mo']
+        },
+
+         {
+             name: 'Saly',
+             imgSrc: 'images/cat2.jpg',
+             imgAttribution: 'https://flickr.come/photos/big',
+             clickCount: 0,
+             nickNames: ['Tabbs', 'Babbs', 'Tabitha', 'mo']
+         },
+
+        {
+            name: 'Bo',
+            imgSrc: 'images/cat3.jpg',
+            imgAttribution: 'https://flickr.come/photos/big',
+            clickCount: 0,
+            nickNames: ['Tabbs', 'Babbs', 'Tabitha', 'mo']
+        },
+
+         {
+             name: 'Ruben',
+             imgSrc: 'images/cat4.jpg',
+             imgAttribution: 'https://flickr.come/photos/big',
+             clickCount: 0,
+             nickNames: ['Tabbs', 'Babbs', 'Tabitha', 'mo']
+         },
+
+        {
+            name: 'Lilly',
+            imgSrc: 'images/cat5.jpg',
+            imgAttribution: 'https://flickr.come/photos/big',
+            clickCount: 0,
+            nickNames: ['Tabbs', 'Babbs', 'Tabitha', 'mo']
+        }
+]
+
+
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttributiuon);
+    this.selected = ko.observable();
+    this.nickName = ko.observableArray(data.nickNames);
 
     this.catLevel = ko.computed(function () {
         return this.clickCount() < 10 ? "Newborn" : (this.clickCount() <= 39 ? "Infant" : (this.clickCount() <= 79 ? "Teen" : "Adult"));
@@ -17,12 +63,19 @@ var Cat = function () {
 }
 
 var ViewModel = function () {
-    this.currentCat = ko.observable(new Cat());
+    var self = this;
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem) {
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
     };
-    //{ clickCount: this.clickCount, name: this.name, imgSrc: this.imgSrc, imgAttribution: this.imgAttribution, nickName: this.nickName, incrementCounter: this.incrementCounter}
+
 
 }
 
