5101ff6ed1134138ab0e2e7ca1d29b0f7fa76714
Hanane Moshe <hananemo@gmail.com>
2015-03-18 15:33:43 -0400

    cleaned html using with binding

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
-var ViewModel = function () {
+var Cat = function () {
     this.clickCount = ko.observable(0);
-    //this.catLevel = ko.observable('Newborn');
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://flickr.come/photos/big');
 
+    this.nickName = ko.observableArray([
+        { nick: 'Tabbs' },
+        { nick: 'Babbs' },
+        { nick: 'Tabitha' },
+        { nick: 'mo' }
+    ]);
+
+    this.catLevel = ko.computed(function () {
+        return this.clickCount() < 10 ? "Newborn" : (this.clickCount() <= 39 ? "Infant" : (this.clickCount() <= 79 ? "Teen" : "Adult"));
+    }, this);
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
     };
+}
 
-    this.catLevel = ko.computed(function () {
-        return this.clickCount() < 10 ? "Newborn" : (this.clickCount() <= 39 ? "Infant": (this.clickCount() <= 79 ? "Teen" : "Adult"));
-    }, this);
+var ViewModel = function () {
+    this.currentCat = ko.observable(new Cat({ clickCount: this.clickCount, name: this.name, imgSrc: this.imgSrc, imgAttribution: this.imgAttribution, nickName: this.nickName, incrementCounter: this.incrementCounter}));
+    //this.currentCat({ clickCount: clickCount, name: name, imgSrc: imgSrc, imgAttribution: imgAttribution, nickName: nickName });
+
 }
 
 ko.applyBindings(new ViewModel)
